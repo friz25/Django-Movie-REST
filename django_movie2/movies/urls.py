289dc4081c9f1b6103e06d7f7824b46b18725e1e
frizmob@gmail.com <friz48625>
2022-06-26 13:34:45 +0300

    v14

--- django_movie2/movies/urls.py
+++ django_movie2/movies/urls.py
@@ -1,15 +1,18 @@
 from django.contrib import admin
 from django.urls import path
+from rest_framework.routers import DefaultRouter # [14]
+from rest_framework.urlpatterns import format_suffix_patterns # [14]
 
 from . import views
 
 app_name = 'movies'
-urlpatterns = [
-    path("movie/", views.MovieListView.as_view()), # http://127.0.0.1:8001/api/v1/movie/
-    path("movie/<int:pk>/", views.MovieDetailView.as_view()), # http://127.0.0.1:8001/api/v1/movie/1
-    path("review/", views.ReviewCreateView.as_view()), # http://127.0.0.1:8001/api/v1/review/
-    path("rating/", views.AddStarRatingView.as_view()), # http://127.0.0.1:8001/api/v1/rating/ {"star":3, "movie": 1}
-    path("actors/", views.ActorsListView.as_view()), # http://127.0.0.1:8001/api/v1/actors/
-    path("actors/<int:pk>", views.ActorsDetailView.as_view()), # http://127.0.0.1:8001/api/v1/actors/1
-
-]
+urlpatterns = format_suffix_patterns([
+    path("movie/", views.MovieViewSet.as_view({'get': 'list'})), # http://127.0.0.1:8001/api/v1/movie/
+    path("movie/<int:pk>/", views.MovieViewSet.as_view({'get': 'retrieve'})), # http://127.0.0.1:8001/api/v1/movie/1
+    path("review/", views.ReviewCreateViewSet.as_view({'post': 'create'})), # http://127.0.0.1:8001/api/v1/review/
+    path("rating/", views.AddStarRatingViewSet.as_view({'post': 'create'})), # http://127.0.0.1:8001/api/v1/rating/ {"star":3, "movie": 1}
+    # path("actors/", views.ActorsListView.as_view()), # http://127.0.0.1:8001/api/v1/actors/
+    # path("actors/<int:pk>", views.ActorsDetailView.as_view()), # http://127.0.0.1:8001/api/v1/actors/1
+    path("actor/", views.ActorsViewSet.as_view({'get': 'list'})), # [14] http://127.0.0.1:8001/api/v1/actor/
+    path("actor/<int:pk>", views.ActorsViewSet.as_view({'get': 'retrieve'})), # [14] http://127.0.0.1:8001/api/v1/actor/1
+])
--- django_movie2/movies/views.py
+++ django_movie2/movies/views.py
@@ -1,5 +1,5 @@
-from django.db import models #дописали [6]
-from rest_framework import generics, permissions
+from django.db import models # дописали [6]
+from rest_framework import generics, permissions, viewsets # [14]
 # from rest_framework.response import Response # удалили [9]
 # from rest_framework.views import APIView # удалили [9]
 from django_filters.rest_framework import DjangoFilterBackend
@@ -14,7 +14,7 @@ from .serializers import (
     ActorDetailSelializer
 )
 from .service import get_client_ip, MovieFilter
-
+'''
 class MovieListView(generics.ListAPIView):
     """ [GET] Вывод списка фильмов [11]"""
     serializer_class = MovieListSerializer
@@ -29,8 +29,32 @@ class MovieListView(generics.ListAPIView):
             middle_star=models.Sum(models.F('ratings__star')) / models.Count(models.F('ratings'))
         )
         return movies
-
 '''
+
+"""###########################################################################
+*ReadOnlyModelViewSet - может выводить и список и одну запись
+###########################################################################"""
+
+class MovieViewSet(viewsets.ReadOnlyModelViewSet):
+    """ [GET] Вывод списка фильмов [14] \n
+    * ReadOnlyModelViewSet - может выводить и список и одну запись """
+    filter_backends = (DjangoFilterBackend,) #подключили фильт django
+    filterset_class = MovieFilter # http://127.0.0.1:8001/api/v1/movie/?year_min=1983&year_max=2022&genres=Боевик
+
+    def get_queryset(self):
+        movies = Movie.objects.filter(draft=False).annotate(
+            rating_user=models.Count("ratings", filter=models.Q(ratings__ip=get_client_ip(self.request)))
+        ).annotate(
+            middle_star=models.Sum(models.F('ratings__star')) / models.Count(models.F('ratings'))
+        )
+        return movies
+
+    def get_serializer_class(self):
+        if self.action == 'list':
+            return MovieListSerializer
+        elif self.action == 'retrieve':
+            return MovieDetailSerializer
+'''[v1]
 class MovieDetailView(APIView):
     """ [GET] Вывод фильма """
     def get(self, request, pk):
@@ -38,11 +62,13 @@ class MovieDetailView(APIView):
         serializer = MovieDetailSerializer(movie)
         return Response(serializer.data)
 '''
+'''[v2]
 class MovieDetailView(generics.RetrieveAPIView):
     """ [GET] Вывод фильма [9]"""
     queryset = Movie.objects.filter(draft=False)
     serializer_class = MovieDetailSerializer
 '''
+'''[v1]
 class ReviewCreateView(APIView):
     """ [POST] Добавление комментария (к фильму) """
     def post(self, request):
@@ -51,10 +77,20 @@ class ReviewCreateView(APIView):
             review.save()
         return Response(status=201)
 '''
+'''[v2]
 class ReviewCreateView(generics.CreateAPIView):
     """ [POST] Добавление комментария (к фильму) [9]"""
     serializer_class = ReviewCreateSerializer
 '''
+
+"""###########################################################################
+*ModelViewSet - позволяет нам реализ-ть сразу добавление, вывод списка, одной записи, обновления, удаления записи 
+###########################################################################"""
+
+class ReviewCreateViewSet(viewsets.ModelViewSet):
+    """ [POST] Добавление комментария (к фильму) [14]"""
+    serializer_class = ReviewCreateSerializer
+'''[v1]
 class AddStarRatingView(APIView):
     """[POST] Добавление рейтинга фильму """
 
@@ -66,6 +102,7 @@ class AddStarRatingView(APIView):
         else:
             return Response(status=400)
 '''
+'''[v2]
 class AddStarRatingView(generics.CreateAPIView):
     """[POST] Добавление рейтинга фильму """
     serializer_class = CreateRatingSerializer
@@ -73,7 +110,15 @@ class AddStarRatingView(generics.CreateAPIView):
     def perform_create(self, serializer):
         """ возвращает IP пользователя """
         serializer.save(ip=get_client_ip(self.request))
+'''
+class AddStarRatingViewSet(viewsets.ModelViewSet):
+    """[POST] Добавление рейтинга фильму """
+    serializer_class = CreateRatingSerializer
 
+    def perform_create(self, serializer):
+        """ возвращает IP пользователя """
+        serializer.save(ip=get_client_ip(self.request))
+'''[v1]
 class ActorsListView(generics.ListAPIView):
     """ Вывод списка актёров [8] """
     queryset = Actor.objects.all()
@@ -83,3 +128,13 @@ class ActorsDetailView(generics.RetrieveAPIView):
     """ Вывод полного описания актёра / режжисёра [8] """
     queryset = Actor.objects.all()
     serializer_class = ActorDetailSelializer
+'''
+class ActorsViewSet(viewsets.ReadOnlyModelViewSet):
+    """" Вывод списка актёров [8] """
+    queryset = Actor.objects.all()
+
+    def get_serializer_class(self):
+        if self.action == 'list':
+            return ActorListSelializer
+        elif self.action == 'retrieve':
+            return ActorDetailSelializer
